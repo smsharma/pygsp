8e0c782af794912ecd701d39c7b761b9e5e12a35
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-02-13 11:40:05 -0500

    Fix deprecated scipy arg

--- pygsp/graphs/nngraphs/nngraph.py
+++ pygsp/graphs/nngraphs/nngraph.py
@@ -16,18 +16,18 @@ _logger = utils.build_logger(__name__)
 
 def _scipy_pdist(features, metric, order, kind, k, radius, params):
     if params:
-        raise ValueError('unexpected parameters {}'.format(params))
-    metric = 'cityblock' if metric == 'manhattan' else metric
-    metric = 'chebyshev' if metric == 'max_dist' else metric
+        raise ValueError("unexpected parameters {}".format(params))
+    metric = "cityblock" if metric == "manhattan" else metric
+    metric = "chebyshev" if metric == "max_dist" else metric
     params = dict(metric=metric)
-    if metric == 'minkowski':
-        params['p'] = order
+    if metric == "minkowski":
+        params["p"] = order
     dist = spatial.distance.pdist(features, **params)
     dist = spatial.distance.squareform(dist)
-    if kind == 'knn':
-        neighbors = np.argsort(dist)[:, :k+1]
+    if kind == "knn":
+        neighbors = np.argsort(dist)[:, : k + 1]
         distances = np.take_along_axis(dist, neighbors, axis=-1)
-    elif kind == 'radius':
+    elif kind == "radius":
         distances = []
         neighbors = []
         for distance in dist:
@@ -39,111 +39,115 @@ def _scipy_pdist(features, metric, order, kind, k, radius, params):
 
 def _scipy_kdtree(features, _, order, kind, k, radius, params):
     if order is None:
-        raise ValueError('invalid metric for scipy-kdtree')
-    eps = params.pop('eps', 0)
+        raise ValueError("invalid metric for scipy-kdtree")
+    eps = params.pop("eps", 0)
     tree = spatial.KDTree(features, **params)
     params = dict(p=order, eps=eps)
-    if kind == 'knn':
-        params['k'] = k + 1
-    elif kind == 'radius':
-        params['k'] = None
-        params['distance_upper_bound'] = radius
+    if kind == "knn":
+        params["k"] = k + 1
+    elif kind == "radius":
+        params["k"] = None
+        params["distance_upper_bound"] = radius
     distances, neighbors = tree.query(features, **params)
     return neighbors, distances
 
 
 def _scipy_ckdtree(features, _, order, kind, k, radius, params):
     if order is None:
-        raise ValueError('invalid metric for scipy-kdtree')
-    eps = params.pop('eps', 0)
+        raise ValueError("invalid metric for scipy-kdtree")
+    eps = params.pop("eps", 0)
     tree = spatial.cKDTree(features, **params)
-    params = dict(p=order, eps=eps, n_jobs=-1)
-    if kind == 'knn':
-        params['k'] = k + 1
-    elif kind == 'radius':
-        params['k'] = features.shape[0]  # number of vertices
-        params['distance_upper_bound'] = radius
+    params = dict(p=order, eps=eps, workers=-1)
+    if kind == "knn":
+        params["k"] = k + 1
+    elif kind == "radius":
+        params["k"] = features.shape[0]  # number of vertices
+        params["distance_upper_bound"] = radius
     distances, neighbors = tree.query(features, **params)
-    if kind == 'knn':
+    if kind == "knn":
         return neighbors, distances
-    elif kind == 'radius':
+    elif kind == "radius":
         dist = []
         neigh = []
         for distance, neighbor in zip(distances, neighbors):
-            mask = (distance != np.inf)
+            mask = distance != np.inf
             dist.append(distance[mask])
             neigh.append(neighbor[mask])
         return neigh, dist
 
 
 def _flann(features, metric, order, kind, k, radius, params):
-    if metric == 'max_dist':
+    if metric == "max_dist":
         raise ValueError('flann gives wrong results for metric="max_dist".')
     try:
         import cyflann as cfl
     except Exception as e:
-        raise ImportError('Cannot import cyflann. Choose another nearest '
-                          'neighbors backend or try to install it with '
-                          'pip (or conda) install cyflann. '
-                          'Original exception: {}'.format(e))
+        raise ImportError(
+            "Cannot import cyflann. Choose another nearest "
+            "neighbors backend or try to install it with "
+            "pip (or conda) install cyflann. "
+            "Original exception: {}".format(e)
+        )
     cfl.set_distance_type(metric, order=order)
     index = cfl.FLANNIndex()
     index.build_index(features, **params)
     # I tried changing the algorithm and testing performance on huge matrices,
     # but the default parameters seems to work best.
-    if kind == 'knn':
-        neighbors, distances = index.nn_index(features, k+1)
-        if metric == 'euclidean':
+    if kind == "knn":
+        neighbors, distances = index.nn_index(features, k + 1)
+        if metric == "euclidean":
             np.sqrt(distances, out=distances)
-        elif metric == 'minkowski':
-            np.power(distances, 1/order, out=distances)
-    elif kind == 'radius':
+        elif metric == "minkowski":
+            np.power(distances, 1 / order, out=distances)
+    elif kind == "radius":
         distances = []
         neighbors = []
-        if metric == 'euclidean':
+        if metric == "euclidean":
             radius = radius**2
-        elif metric == 'minkowski':
+        elif metric == "minkowski":
             radius = radius**order
         n_vertices, _ = features.shape
         for vertex in range(n_vertices):
             neighbor, distance = index.nn_radius(features[vertex, :], radius)
             distances.append(distance)
             neighbors.append(neighbor)
-        if metric == 'euclidean':
+        if metric == "euclidean":
             distances = list(map(np.sqrt, distances))
-        elif metric == 'minkowski':
-            distances = list(map(lambda d: np.power(d, 1/order), distances))
+        elif metric == "minkowski":
+            distances = list(map(lambda d: np.power(d, 1 / order), distances))
     index.free_index()
     return neighbors, distances
 
 
 def _nmslib(features, metric, order, kind, k, _, params):
-    if kind == 'radius':
+    if kind == "radius":
         raise ValueError('nmslib does not support kind="radius".')
-    if metric == 'minkowski':
+    if metric == "minkowski":
         raise ValueError('nmslib does not support metric="minkowski".')
     try:
         import nmslib as nms
     except Exception as e:
-        raise ImportError('Cannot import nmslib. Choose another nearest '
-                          'neighbors backend or try to install it with '
-                          'pip (or conda) install nmslib. '
-                          'Original exception: {}'.format(e))
+        raise ImportError(
+            "Cannot import nmslib. Choose another nearest "
+            "neighbors backend or try to install it with "
+            "pip (or conda) install nmslib. "
+            "Original exception: {}".format(e)
+        )
     n_vertices, _ = features.shape
-    params_index = params.pop('index', None)
-    params_query = params.pop('query', None)
-    metric = 'l2' if metric == 'euclidean' else metric
-    metric = 'l1' if metric == 'manhattan' else metric
-    metric = 'linf' if metric == 'max_dist' else metric
+    params_index = params.pop("index", None)
+    params_query = params.pop("query", None)
+    metric = "l2" if metric == "euclidean" else metric
+    metric = "l1" if metric == "manhattan" else metric
+    metric = "linf" if metric == "max_dist" else metric
     index = nms.init(space=metric, **params)
     index.addDataPointBatch(features)
     index.createIndex(params_index)
     if params_query is not None:
         index.setQueryTimeParams(params_query)
-    results = index.knnQueryBatch(features, k=k+1)
+    results = index.knnQueryBatch(features, k=k + 1)
     neighbors, distances = zip(*results)
-    distances = np.concatenate(distances).reshape(n_vertices, k+1)
-    neighbors = np.concatenate(neighbors).reshape(n_vertices, k+1)
+    distances = np.concatenate(distances).reshape(n_vertices, k + 1)
+    neighbors = np.concatenate(neighbors).reshape(n_vertices, k + 1)
     return neighbors, distances
 
 
@@ -346,12 +350,20 @@ class NNGraph(Graph):
     >>> _ = axes[1].legend(loc='upper left')
     """
 
-    def __init__(self, features, standardize=False,
-                 metric='euclidean', order=3,
-                 kind='knn', k=10, radius='estimate-knn',
-                 kernel='gaussian', kernel_width=None,
-                 backend='scipy-ckdtree',
-                 **kwargs):
+    def __init__(
+        self,
+        features,
+        standardize=False,
+        metric="euclidean",
+        order=3,
+        kind="knn",
+        k=10,
+        radius="estimate-knn",
+        kernel="gaussian",
+        kernel_width=None,
+        backend="scipy-ckdtree",
+        **kwargs
+    ):
 
         # features is stored in coords, potentially standardized
         self.standardize = standardize
@@ -363,11 +375,11 @@ class NNGraph(Graph):
 
         features = np.asanyarray(features)
         if features.ndim != 2:
-            raise ValueError('features should be #vertices x dimensionality')
+            raise ValueError("features should be #vertices x dimensionality")
         n_vertices, dimensionality = features.shape
 
         params_graph = dict()
-        for key in ['lap_type', 'plotting']:
+        for key in ["lap_type", "plotting"]:
             try:
                 params_graph[key] = kwargs.pop(key)
             except KeyError:
@@ -380,63 +392,75 @@ class NNGraph(Graph):
 
         # Order consistent with metric (used by kdtree and ckdtree).
         _orders = {
-            'euclidean': 2,
-            'manhattan': 1,
-            'max_dist': np.inf,
-            'minkowski': order,
+            "euclidean": 2,
+            "manhattan": 1,
+            "max_dist": np.inf,
+            "minkowski": order,
         }
         order = _orders.pop(metric, None)
 
-        if kind == 'knn':
+        if kind == "knn":
             if not 1 <= k < n_vertices:
-                raise ValueError('The number of neighbors (k={}) must be '
-                                 'greater than 0 and smaller than the number '
-                                 'of vertices ({}).'.format(k, n_vertices))
+                raise ValueError(
+                    "The number of neighbors (k={}) must be "
+                    "greater than 0 and smaller than the number "
+                    "of vertices ({}).".format(k, n_vertices)
+                )
             radius = None
-        elif kind == 'radius':
-            if radius == 'estimate':
+        elif kind == "radius":
+            if radius == "estimate":
                 maximums = np.amax(features, axis=0)
                 minimums = np.amin(features, axis=0)
                 distance_max = np.linalg.norm(maximums - minimums, order)
-                radius = distance_max / np.power(n_vertices, 1/dimensionality)
-            elif radius == 'estimate-knn':
-                graph = NNGraph(features, standardize=standardize,
-                                metric=metric, order=order, kind='knn', k=k,
-                                kernel_width=None, backend=backend, **kwargs)
+                radius = distance_max / np.power(n_vertices, 1 / dimensionality)
+            elif radius == "estimate-knn":
+                graph = NNGraph(
+                    features,
+                    standardize=standardize,
+                    metric=metric,
+                    order=order,
+                    kind="knn",
+                    k=k,
+                    kernel_width=None,
+                    backend=backend,
+                    **kwargs
+                )
                 radius = graph.kernel_width
             elif radius <= 0:
-                raise ValueError('The radius must be greater than 0.')
+                raise ValueError("The radius must be greater than 0.")
             self.k = None
         else:
             raise ValueError('Invalid kind "{}".'.format(kind))
 
         try:
-            function = globals()['_' + backend.replace('-', '_')]
+            function = globals()["_" + backend.replace("-", "_")]
         except KeyError:
             raise ValueError('Invalid backend "{}".'.format(backend))
-        
 
-        neighbors, distances = function(features, metric, order,
-                                        kind, k, radius, kwargs)
+        neighbors, distances = function(
+            features, metric, order, kind, k, radius, kwargs
+        )
         # ------ MARTINO's MODIFICATION ------
         self.distances = distances
         # ------------------------------------
-        
+
         n_edges = [len(n) - 1 for n in neighbors]  # remove distance to self
 
-        if kind == 'radius':
+        if kind == "radius":
             n_disconnected = np.sum(np.asarray(n_edges) == 0)
             if n_disconnected > 0:
-                _logger.warning('{} vertices (out of {}) are disconnected. '
-                                'Consider increasing the radius or setting '
-                                'kind=knn.'.format(n_disconnected, n_vertices))
+                _logger.warning(
+                    "{} vertices (out of {}) are disconnected. "
+                    "Consider increasing the radius or setting "
+                    "kind=knn.".format(n_disconnected, n_vertices)
+                )
 
         value = np.empty(sum(n_edges), dtype=np.float)
         row = np.empty_like(value, dtype=np.int)
         col = np.empty_like(value, dtype=np.int)
         start = 0
         for vertex in range(n_vertices):
-            if kind == 'knn':
+            if kind == "knn":
                 assert n_edges[vertex] == k
             end = start + n_edges[vertex]
             value[start:end] = distances[vertex][1:]
@@ -447,24 +471,24 @@ class NNGraph(Graph):
 
         # Enforce symmetry. May have been broken by k-NN. Checking symmetry
         # with np.abs(W - W.T).sum() is as costly as the symmetrization itself.
-        W = utils.symmetrize(W, method='fill')
+        W = utils.symmetrize(W, method="fill")
 
         if kernel_width is None:
-            if kind == 'knn':
+            if kind == "knn":
                 kernel_width = np.mean(W.data) if W.nnz > 0 else np.nan
-            elif kind == 'radius':
+            elif kind == "radius":
                 kernel_width = radius / 2
 
         if not callable(kernel):
             try:
                 kernel = self._kernels[kernel]
             except KeyError:
-                raise ValueError('Unknown kernel {}.'.format(kernel))
+                raise ValueError("Unknown kernel {}.".format(kernel))
 
-        assert np.all(W.data >= 0), 'Distance must be in [0, inf].'
+        assert np.all(W.data >= 0), "Distance must be in [0, inf]."
         W.data = kernel(W.data / kernel_width)
         if not np.all((W.data >= 0) & (W.data <= 1)):
-            _logger.warning('Kernel returned similarity not in [0, 1].')
+            _logger.warning("Kernel returned similarity not in [0, 1].")
 
         self.order = order
         self.radius = radius
@@ -474,20 +498,22 @@ class NNGraph(Graph):
 
     def _get_extra_repr(self):
         attrs = {
-            'standardize': self.standardize,
-            'metric': self.metric,
-            'order': self.order,
-            'kind': self.kind,
+            "standardize": self.standardize,
+            "metric": self.metric,
+            "order": self.order,
+            "kind": self.kind,
         }
         if self.k is not None:
-            attrs['k'] = self.k
+            attrs["k"] = self.k
         if self.radius is not None:
-            attrs['radius'] = '{:.2e}'.format(self.radius)
-        attrs.update({
-            'kernel': '{}'.format(self.kernel),
-            'kernel_width': '{:.2e}'.format(self.kernel_width),
-            'backend': self.backend,
-        })
+            attrs["radius"] = "{:.2e}".format(self.radius)
+        attrs.update(
+            {
+                "kernel": "{}".format(self.kernel),
+                "kernel_width": "{:.2e}".format(self.kernel_width),
+                "backend": self.backend,
+            }
+        )
         return attrs
 
     @staticmethod
@@ -506,9 +532,9 @@ class NNGraph(Graph):
 
     @staticmethod
     def _kernel_powers(distance, pow1, pow2, value_at_one=0.5):
-        cst = (1 - value_at_one**(1/pow2))**(1/pow1)
+        cst = (1 - value_at_one ** (1 / pow2)) ** (1 / pow1)
         distance = np.clip(cst * distance, 0, 1)
-        return (1 - distance**pow1)**pow2
+        return (1 - distance**pow1) ** pow2
 
     @staticmethod
     def _kernel_logistic(distance, value_at_one=0.5):
@@ -525,17 +551,14 @@ class NNGraph(Graph):
         return 2 / (np.exp(distance) + np.exp(-distance))
 
     _kernels = {
-        'rectangular': _kernel_rectangular.__func__,
-        'triangular': _kernel_triangular.__func__,
-
-        'exponential': _kernel_exponential.__func__,
-        'gaussian': partial(_kernel_exponential.__func__, power=2),
-
-        'tricube': partial(_kernel_powers.__func__, pow1=3, pow2=3),
-        'triweight': partial(_kernel_powers.__func__, pow1=2, pow2=3),
-        'quartic': partial(_kernel_powers.__func__, pow1=2, pow2=2),
-        'epanechnikov': partial(_kernel_powers.__func__, pow1=2, pow2=1),
-
-        'logistic': _kernel_logistic.__func__,
-        'sigmoid': _kernel_sigmoid.__func__,
+        "rectangular": _kernel_rectangular.__func__,
+        "triangular": _kernel_triangular.__func__,
+        "exponential": _kernel_exponential.__func__,
+        "gaussian": partial(_kernel_exponential.__func__, power=2),
+        "tricube": partial(_kernel_powers.__func__, pow1=3, pow2=3),
+        "triweight": partial(_kernel_powers.__func__, pow1=2, pow2=3),
+        "quartic": partial(_kernel_powers.__func__, pow1=2, pow2=2),
+        "epanechnikov": partial(_kernel_powers.__func__, pow1=2, pow2=1),
+        "logistic": _kernel_logistic.__func__,
+        "sigmoid": _kernel_sigmoid.__func__,
     }
